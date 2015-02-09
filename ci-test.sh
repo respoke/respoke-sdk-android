0ae743c0149de8edfb11d5adf61a72a441e11ac1
David M. Lee <dlee@digium.com>
2015-02-09 15:05:16 -0600

    Adding ci-test script
    The script for running the tests on Bamboo are sufficiently complicated
    that it's best to check them into source control.

---
+++ ci-test.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+#
+# Test runner for the CI server
+#
+
+trap "on_exit" EXIT
+
+# Ensure cleanup runs on exit
+function on_exit()
+{
+  if test -f chrome.pid; then
+    kill $(cat chrome.pid)
+    rm -f chrome.pid
+  fi
+}
+
+set -ex
+
+# Kill lingering instances of Google Chrome
+killall -9 "Google Chrome" || true # ignore errors
+while test $(ps aux | grep ^bamboo | grep -i [c]hrome | wc -l) -ne 0; do
+  sleep 1
+done
+
+# Launch Google Chrome
+"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" \
+    --use-fake-ui-for-media-stream \
+    --use-fake-device-for-media-stream \
+    --allow-file-access-from-files \
+    ./WebTestBot/index.html &
+echo $! > chrome.pid
+
+# Run the tests
+./gradlew clean connectedAndroidTest
